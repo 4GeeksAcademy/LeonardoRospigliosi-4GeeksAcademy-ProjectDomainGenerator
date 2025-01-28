b960d730c2f1401214d93a4032ecdd5993676537
Leonardo Rospigliosi <70114762+LeonardoRospigliosi@users.noreply.github.com>
2025-01-28 04:02:02 +0000

    commited leo

---
+++ app.js
@@ -0,0 +1,22 @@
+
+    let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
+    let extension = ['.com', '.net', '.us', '.io'];
+
+    let mensaje = '';
+        
+
+    for (let i = 0; i < pronoun.length; i++) {
+        for (let j = 0; j < adj.length; j++) {
+            for (let k = 0; k < noun.length; k++) {
+                for (let m = 0; m < extension.length; m++) {
+                    mensaje= pronoun[i] + adj[j]+ noun[k]+ extension[m];
+                    console.log(mensaje);
+                }            
+            }
+        }
+    }
+
+    
+  
